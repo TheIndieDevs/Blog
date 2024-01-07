613ba674076d215ba2f95667dec40f32132ea993
Chuan Chen <chuan.chen402@qmail.cuny.edu>
2024-01-07 10:35:19 -0500

    updated backend

--- server/index.js
+++ server/index.js
@@ -1,20 +1,25 @@
 
 const express = require('express');
-
+const app = express();
+const cors = require('cors');
 const dotenv = require('dotenv');
 
 const PORT = process.env.port || 3000;
 
 dotenv.config();
 
-express().use(cors());
-express().use(express.json());
+app.use(cors());
+app.use(express.json());
 
-express().use((req, res, next) => {
+app.use( '/', (req, res)=>{
+    res.send('backend opened');
+});
+
+app.use((req, res, next) => {
     res.status(404).send('Page Not Found');
 })
 
 
-express().listen(PORT, ()=> {
-    console.log(`Server is running on http://localhost${PORT}`)
+app.listen(PORT, ()=> {
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
