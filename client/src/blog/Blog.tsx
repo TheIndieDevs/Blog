1c419fe20647879ac35edc407e6c508b12d9e702
Chuan Chen <chuan.chen402@qmail.cuny.edu>
2024-01-16 10:11:26 -0500

    error persists

--- client/src/blog/Blog.tsx
+++ client/src/blog/Blog.tsx
@@ -12,9 +12,7 @@ import FeaturedPost from './FeaturedPost';
 import Main from './Main';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
-import post1 from './blog-post.1.md';
-import post2 from './blog-post.2.md';
-import post3 from './blog-post.3.md';
+
 
 const sections = [
   { title: 'Technology', url: '#' },
@@ -57,7 +55,7 @@ const featuredPosts = [
   },
 ];
 
-const posts = [post1, post2, post3];
+const posts = [];
 
 const sidebar = {
   title: 'About',
