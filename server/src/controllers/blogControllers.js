19579cf3d580412b1bf5ee36f71576a584070c73
Whykari <61473858+Chuan-Chen@users.noreply.github.com>
2024-01-26 22:45:17 -0500

    updated createBlog controller

--- server/src/controllers/blogControllers.js
+++ server/src/controllers/blogControllers.js
@@ -1,7 +1,24 @@
 const Blog = require('../models/blog');
+const Blog = require('../models/blog');
 
 exports.createBlog = async (req, res) => {
+    try{
+        const {title, content, author, dateCreated, dateModified, comments} = req.body;
 
+        let blog = new Blog({
+            title,
+            content,
+            author,
+            dateCreated: dateModified || new Date(),
+            dateModified: dateCreated || dateModified,
+            comments
+        })
+        await blog.save();
+        res.status(201).json({message: "blog created"});
+
+    }catch(error){
+        res.status(500).json({message: "blog not created"})
+    }
 }
 
 exports.updateBlog = async (req, res) => {
